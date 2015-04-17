4953b20dae41752f83cfb3858d6d4484d580cef7
Brian Choy <bycEEE@gmail.com>
2015-04-17 10:31:57 -0400

    Update playlists views

---
+++ app/controllers/playlists_controller.rb
@@ -0,0 +1,24 @@
+class PlaylistsController < ApplicationController
+  def index
+    @playlists = Playlist.all
+  end
+
+  def show
+    @client = YouTubeIt::Client.new(:dev_key => ENV['YOUTUBE_KEY'])
+    @playlist = Playlist.find(params[:id])
+  end
+
+  def new
+    @playlist = Playlist.new
+  end
+
+  def create
+    @playlist = Playlist.create(playlist_params)
+    redirect_to @playlist
+  end
+
+  private
+  def playlist_params
+    params.require(:playlist).permit(:name)
+end
+end
