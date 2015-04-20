2e80338a03e8b43f30b3a5800783095505b92e62
Brian Choy <bycEEE@gmail.com>
2015-04-20 10:31:13 -0400

    Add songs to YouTube player via URL

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,14 +1,12 @@
 class SongsController < ApplicationController
 
-	def create
-		binding.pry
-  	song = Song.find_or_create_by(Song.get_song_hash_from_url(params[:song][:url]))
-  	request = Request.find_or_create_by(
-  	{	:song_id => song.id, 
-  		:listener_id => params[:song][:user_id], 
-  		:playlist_id => params[:song][:playlist_id] })
-  	playlist = Playlist.find(params[:song][:playlist_id])
-  	redirect_to playlist
+  def create
+    song = Song.find_or_create_by(Song.get_song_hash_from_url(params[:song][:url]))
+    request = Request.find_or_create_by(
+    {	:song_id => song.id, 
+      :listener_id => params[:song][:user_id], 
+      :playlist_id => params[:song][:playlist_id] })
+      playlist = Playlist.find(params[:song][:playlist_id])
+      # redirect_to playlist
   end
-
 end
