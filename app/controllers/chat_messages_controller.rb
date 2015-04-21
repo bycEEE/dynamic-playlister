3a8bffaf72b82d3f743fbcbb9348ff9c63e1493f
lewaabahmad <lewaabahmad@gmail.com>
2015-04-20 23:37:50 -0400

    add user to chat box

--- app/controllers/chat_messages_controller.rb
+++ app/controllers/chat_messages_controller.rb
@@ -2,8 +2,10 @@ class ChatMessagesController < ApplicationController
 	layout false
 	def create
 		@chat_message = ChatMessage.create(chat_message_params)
+		broadcast_information = { :content => "#{@chat_message.content}", :user => "#{@chat_message.user.name}" }
 		# Temp.broadcast("/chat_messages/#{@chat_message.playlist.id}", @chat_message)
-		Temp.broadcast("/chat_messages/#{@chat_message.playlist.id}", @chat_message)
+		# Temp.broadcast("/chat_messages/#{@chat_message.playlist.id}", @chat_message)
+		Temp.broadcast("/chat_messages/#{@chat_message.playlist.id}", broadcast_information)
 		render :nothing => true
 	end
 
