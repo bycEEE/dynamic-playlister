27a3dbddfe50a743c001b3adfbe4c30addeeb2aa
Brian Choy <bycEEE@gmail.com>
2015-04-23 10:55:25 -0400

    Fix delete to delete from YouTube video playlist

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -19,7 +19,7 @@ class RequestsController < ApplicationController
 
   def destroy
     request = Request.find(params[:request_id])
-    broadcast_information = { :request_id => "#{request.id}" }
+    broadcast_information = { :request_id => "#{request.id}", :request_song_uid => "#{request.song.uid}" }
     request.destroy
     FayeServer.broadcast("/playlists/#{request.playlist.id}/delete", broadcast_information)
     render :nothing => true
